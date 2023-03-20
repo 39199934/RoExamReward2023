8da23b59fb305baf8d36d0ba62049e373c38d957
rolodestar <39199934@qq.com>
2023-03-20 19:17:57 +0800

    Initial Commit

---
+++ RoExamReward2023/RoExamReward2023App.swift
@@ -0,0 +1,20 @@
+//
+//  RoExamReward2023App.swift
+//  RoExamReward2023
+//
+//  Created by rolodestar on 2023/3/20.
+//
+
+import SwiftUI
+
+@main
+struct RoExamReward2023App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
